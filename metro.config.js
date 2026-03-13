e96ee650997afdc893024e0447488e17dcd67fba
Gdm0714 <ehdals45454@naver.com>
2026-03-13 20:46:14 +0900

    fix: Metro에서 @toss/tds-react-native/private 서브패스 resolve 설정

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
@@ -6,6 +7,17 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // Metro가 package.json exports 서브패스를 resolve하지 못하는 문제 해결
+    extraNodeModules: {
+      '@toss/tds-react-native/private': path.resolve(
+        __dirname,
+        'node_modules/@toss/tds-react-native/dist/cjs/private',
+      ),
+    },
+    unstable_enablePackageExports: true,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
